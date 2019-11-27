0b3dbf9054d64dfa1fef10b5e46918b1345352c6
Zac Smith <zrs3141592@gmail.com>
2019-11-27 16:04:58 -0700

    create node server

--- express101/nodeServer.js
+++ express101/nodeServer.js
@@ -1,2 +1,16 @@
-const http = require('http')
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  // console.log(req.headers.host);
+
+  // writehead takes 2 args
+  // 1. status code
+  // 2. object for mime-type
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>Node server is running</h1>");
+  res.end();
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`\n *** Server is listening on port ${PORT} *** \n`);
+});

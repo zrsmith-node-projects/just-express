f4f18e2dd08d8f454cc7fc9656a20473b802d700
Zac Smith <zrs3141592@gmail.com>
2019-11-28 12:03:13 -0700

    update

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - `npm init`
 - `npx gitignore node`
+- `curl -v localhost:3000` to view the headers created
--- express101/nodeServer.js
+++ express101/nodeServer.js
@@ -1,15 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(req.headers.host);
+  console.log(req.url);
+
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    // res.write("<h1>Node server is running</h1>");
+    const homePageHTML = fs.readFileSync("node.html");
+    res.write(homePageHTML);
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write(
+      `<h4>Sorry, ${req.url}, this is not the page you are looking for!</h4>`
+    );
+    res.end();
+  }
 
   // writehead takes 2 args
   // 1. status code
   // 2. object for mime-type
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Node server is running</h1>");
-  res.end();
+  // res.writeHead(200, { "content-type": "text/html" });
+  // res.write("<h1>Node server is running</h1>");
+  // res.end();
 });
+
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`\n *** Server is listening on port ${PORT} *** \n`);

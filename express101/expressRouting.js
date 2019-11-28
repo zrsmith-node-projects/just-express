e1f0adba6a3dc1765e4f44742b90a56ea86a89fc
Zac Smith <zrs3141592@gmail.com>
2019-11-28 14:20:41 -0700

    finish expressRouting to match nodeServer functionality

--- express101/expressRouting.js
+++ express101/expressRouting.js
@@ -1,10 +1,14 @@
+const path = require("path");
 const express = require("express");
 const server = express();
-
+server.use(express.static("public"));
 server.all("/", (req, res) => {
-  res.send(`<h1>Node server is running</h1>`);
+  // res.send(`<h1>Node server is running</h1>`);
+  res.sendFile(path.join(__dirname + "/node.html"));
 });
-server.get("/", (req, res) => {});
+server.all("*", (req, res) => {
+  res.send(`This page does not exist!`);
+});
 
 const PORT = 3555;
 server.listen(PORT, () => {
--- express101/staticFiles.js
+++ express101/staticFiles.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = express();
 
+server.use(express.static("public"));
+
 const PORT = 3666;
 server.listen(PORT, () => {
   console.log(`\n *** Server listening on port ${PORT} *** \n`);

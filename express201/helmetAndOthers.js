e14199580bacba282b78c4da1280ddae9180d193
Zac Smith <zrs3141592@gmail.com>
2019-11-28 18:31:53 -0700

    ajax call to server

--- express201/helmetAndOthers.js
+++ express201/helmetAndOthers.js
@@ -2,9 +2,12 @@ const express = require("express");
 const server = express();
 
 server.use(express.static("public"));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 server.post("/ajax", (req, res) => {
-  res.send("Test");
+  console.log(req.body);
+  res.send("Test!");
 });
 
 const PORT = 3333;

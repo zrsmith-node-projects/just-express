3a1ace39946c368cdd22c97b13629bad8acf0c95
Zac Smith <zrs3141592@gmail.com>
2019-11-29 14:20:18 -0700

    create view engine

--- express201/rendering.js
+++ express201/rendering.js
@@ -1,13 +1,18 @@
+const path = require("path");
 const express = require("express");
 
 const server = express();
-
+const helmet = require("helmet");
+server.use(helmet());
 server.use(express.static("public"));
 server.use(express.json());
 server.use(express.urlencoded());
 
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "views"));
+
 server.get("/", (req, res) => {
-  res.send("Testing node / express");
+  res.render("index");
 });
 
 const PORT = 3333;

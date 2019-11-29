a8dcb643c606dd7b81d8c582e446a8b66a7914c1
Zac Smith <zrs3141592@gmail.com>
2019-11-29 13:54:56 -0700

    add standard .use()

--- express201/rendering.js
+++ express201/rendering.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.static("public"));
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.get("/", (req, res) => {});
+
 const PORT = 3333;
 server.listen(PORT, () => {
   console.log(`\n *** Server is listening on port ${PORT} *** \n`);

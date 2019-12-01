0afd246759ac55233e20a303fc6dce3f7f6e0b80
Zac Smith <zrs3141592@gmail.com>
2019-11-30 18:44:58 -0700

    create log in and log out functions using cookieParser

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
-
+const cookieParser = require("cookie-parser");
 const server = express();
 
 server.use(helmet());
 server.use(express.static("public"));
 server.use(express.json());
 server.use(express.urlencoded());
+server.use(cookieParser());
 
 server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "views"));
@@ -23,7 +24,31 @@ server.get("/login", (req, res) => {
 });
 
 server.post("/process_login", (req, res) => {
-  res.status(200).json(req.body);
+  const user = req.body.username;
+  const pwd = req.body.password;
+  // res.cookie takes 2 args: 1, name of the cookie, 2. value to set to it
+
+  if (pwd === "z") {
+    res.cookie("user", user);
+    res.redirect("/welcome");
+  } else {
+    res.redirect("/invalid?msg=fail");
+  }
+
+  // res.status(200).json(req.body);
+});
+
+server.get("/welcome", (req, res) => {
+  res.render("welcome", {
+    user: req.cookies.user
+  });
+});
+server.get("/logout", (req, res) => {
+  res.clearCookie("user");
+  res.redirect("login");
+});
+server.get("/invalid", (req, res) => {
+  res.render("invalid");
 });
 
 const PORT = 3333;

1c1a596837543b3e9e4cf5545cf09cf166ffbc87
Zac Smith <zrs3141592@gmail.com>
2019-11-30 17:58:35 -0700

    fix login css

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -9,13 +9,17 @@ server.use(express.static("public"));
 server.use(express.json());
 server.use(express.urlencoded());
 
-server.set("view_engine", "ejs");
+server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "views"));
 
 server.get("/", (req, res) => {
   res.status(200).json({
     message: `NodeJS server with ExpressJS is running on ${PORT}`
   });
+});
+
+server.get("/login", (req, res) => {
+  res.render("login");
 });
 
 const PORT = 3333;

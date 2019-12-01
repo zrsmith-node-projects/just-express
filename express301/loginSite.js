6695933dcd23e704322987c61f7d384274f5b09e
Zac Smith <zrs3141592@gmail.com>
2019-11-30 17:44:49 -0700

    create server

---
+++ express301/loginSite.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.static("public"));
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.set("view_engine", "ejs");
+server.set("views", path.join(__dirname, "views"));
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: `NodeJS server with ExpressJS is running on ${PORT}`
+  });
+});
+
+const PORT = 3333;
+server.listen(PORT, () => {
+  console.log(`\n *** Server is listening on port ${PORT} *** \n`);
+});
